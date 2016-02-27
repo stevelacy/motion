e439c54ba57bf9bd48c76b027bd2054704c91757
steelbrain <de.steelbrain@gmail.com>
2016-02-27 13:50:34 -0700

    :new: Open in editor work in new CLI

--- packages/motion/src/runner/index.js
+++ packages/motion/src/runner/index.js
@@ -1,11 +1,16 @@
 'use strict'
 
+import FS from 'fs'
+import promisify from 'sb-promisify'
 import open from 'open'
 import chalk from 'chalk'
+import { exec } from 'sb-exec'
 import { CLI } from './cli'
 import { run as runStartup, build } from './startup'
 import server from './server'
 import builder from './builder'
+
+const realPath = promisify(FS.realpath)
 
 // print - so we can easily weed out console.logs
 // print = we want to log this out, keep it
@@ -23,7 +28,8 @@ async function run(options) {
     open('http://' + server.url())
   })
   cli.addCommand('editor', 'Open this project in Atom', async function() {
-    this.log('I should have opened this project in Atom')
+    const directory = await realPath(process.cwd())
+    await exec('atom', [directory])
   })
   cli.addCommand('build', 'Build dist files of your motion app', async function() {
     await builder.build()
